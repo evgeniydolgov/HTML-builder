b3b3a563e4d694926d56947068bd27473dbd7d2e
evgeniydolgov <amyr214519@gmail.com>
2022-11-07 11:25:46 +0300

    fix: fixed six task solution

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,64 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-
-
-
-// читаем файл
-// fs.readFile(path.join(__dirname, "./template.html"), (e, file) => {
-//     if (e) throw new Error(e);
-//     file = file.toString(); // конвектируем в строку
-//     file = file.replace('{{header}}', fileText('articles.html'));
-
-//     fs.writeFile(path.join(__dirname, "./template.html"), file, () => {}); // записываем обратно
-// })
-
-
-
-
-//    fs.readFile(path.join(__dirname, 'components', 'articles.html'), 'utf-8', (err, data ) => {
-//         let answer = data.toString();
-//         console.log(answer);
-//         return answer;
-//     })
-   
-
-    // fs.readdir(path.join(__dirname, './files-copy'), (err, data) => {
-    //     data.forEach((el) => {
-    //         const a = path.join(__dirname, './files-copy', el.toString())
-    //         fs.unlink(a, (err) => {
-    //         });
-    //     })
-    //     copyFiles();
-    // })
-
-
-const createdDirectory = async () => {
-    return new Promise((resolve, reject) => {
-        fs.mkdir((__dirname, './06-build-page/project-dist'), () => {
-            resolve();
-        })
-    })
-}
-
 const cleaningDirectory = async () => {
     return new Promise((resolve, reject) => {
-        fs.readdir (path.join(__dirname, './project-dist'), (err, data) => {
-           if(data.length > 0){
-            data.forEach((el) => {
-                fs.unlink(path.join(__dirname, './project-dist', el.toString()), (err) => {
-                    resolve();
-                });
-            })}
-            fs.copyFile(path.join(__dirname, "template.html"), path.join(__dirname, "./project-dist/index.html"),()=>{})
-        })
+            fs.rm(path.join(__dirname, 'project-dist'), { recursive: true, force: true }, ()=>{
+                fs.mkdir(path.join(__dirname, 'project-dist'), () => {    
+                    createdIndexHtml()                
+                })                
+            })
     })
 }
 
 const createdIndexHtml = async () => { // тести без нее
     return new Promise((resolve, reject) => {
-        fs.copyFile(path.join(__dirname, "template.html"), path.join(__dirname, "./project-dist/index.html"),()=>{})
-        resolve ();
+        fs.copyFile(path.join(__dirname, "template.html"), path.join(__dirname, 'project-dist', "index.html"),()=>{
+            writeIndexHtml();
+        })
     })
 }
 
@@ -82,6 +39,7 @@ const writeIndexHtml = async () => {
 
                 })
                 }
+                createdImageDirectory()
             }
             htmlConstructor();
         })
@@ -103,45 +61,33 @@ const createdStyles = () => {
     })
 }
 
-
-
-
-
-
-
-
-
-
-
-function copyFiles() {
-    fs.copyFile(path.join(__dirname,'./files', elem), path.join(__dirname, './files-copy', elem),() => {})
+const createdImageDirectory = () => {
+    return new Promise((resolve, reject) => {
+        fs.mkdir((__dirname, './06-build-page/project-dist/assets'), () => {
+            copyImage();
+        })
+    })
 }
 
+const copyImage = () => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(path.join(__dirname, "assets"),{withFileTypes: true}, (err, data) => {
+            data.forEach((el) => {
+                if(el.isDirectory()){
+                    fs.mkdir(path.join(__dirname, "project-dist/assets", el.name), () => {
+                    fs.readdir(path.join(__dirname, "assets", el.name), (err, file) => {
+                        file.forEach((elem) => {
+                            fs.copyFile(path.join(__dirname,'assets', el.name, elem), path.join(__dirname, 'project-dist/assets', el.name, elem),() => {})
+                        })
+                    })
+                })
+                } else {
+                    fs.copyFile(path.join(__dirname,'assets', el.name), path.join(__dirname, 'project-dist/assets', el.name),() => {})
+                }
+            })
+        })
+    })
+}
 
-createdDirectory()
-    .then(cleaningDirectory())
-    .then(writeIndexHtml())
+cleaningDirectory()
     .then(createdStyles())
-
-
-    
-   
-
-
-    
-
-// пример записи промисов    
-
-// const writeFileAsync = async (path,data) => {
-//     return new Promise((resolve, reject) => fs.writeFile(
-//         path,
-//         data,
-//         (err, data ) => {
-//             if (err) {
-//                 return reject(err.message)
-//             }
-//             resolve()
-//     }))
-// }
-// writeFileAsync(path.join(__dirname, 'components', 'aaa.html'),'data')
-//     .then(() => writeFileAsync(path.join(__dirname, 'components', 'aaa.html'), 'тексвыва'))
